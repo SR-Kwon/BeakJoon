d69664ef8c8aaaf57c95df52b230b3296ca59108
SR-Kwon <ksr9458@hufs.ac.kr>
2023-09-08 00:15:14 +0900

    9/8

--- 10093.py
+++ 10093.py
@@ -1,13 +1,19 @@
-#같은수일때 생각해보자 아직 못품
-
-low, high = map(int, input().split())
-
-if low > high:
-    temp = low
-    low = high
-    high = temp
+def 사이_수_출력(low, high):
+    temp = []
+    for i in range(high - low - 1):
+        temp.append(low + i + 1)
+        
+    print(' '.join(map(str, temp)))
     
-print(high - low - 1)
-for i in range(high - low - 2):
-    print(low + i + 1, end=' ')
-print(high - 1)
+    
+
+num1, num2 = map(int, input().split())
+
+if num1 < num2:
+    print(num2 - num1 - 1)
+    사이_수_출력(num1, num2)
+elif num2 < num1:
+    print(num1 - num2 - 1)
+    사이_수_출력(num2, num1)
+else:
+    print(0)
