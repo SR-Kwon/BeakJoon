d4bb44c916619c0b2be2eb05633dc163ea92c723
SR-Kwon <ksr9458@hufs.ac.kr>
2023-09-08 13:53:49 +0900

    9/8

--- 1267.py
+++ 1267.py
@@ -6,15 +6,16 @@
 동호가 저번 달에 새악대로 T 통신사를 이용할 때 통화 시간 목록이 주어지면 어느 요금제를 사용 하는 것이 저렴한지 출력하는 프로그램을 작성하시오.
 '''
 
-#0시간 일때도 고려해야함
-#개판이네 다시짜자
-
 def compare(times):
+    price_영식, price_민식 = 0, 0
     for time in times:
-        price_영식 += (time // 30) * 10
-        price_민식 += (time // 60) * 15
-        print(price_민식,price_영식)
-        
+        if time <= 0:
+            price_민식 += 0
+            price_영식 += 0
+        else:
+            price_영식 += ((time // 30) + 1) * 10
+            price_민식 += ((time // 60) + 1) * 15        
+                
     if price_영식 > price_민식:
         print('M', price_민식)
     elif price_영식 < price_민식:
@@ -22,8 +23,6 @@ def compare(times):
     else:
         print('Y M', price_영식)
         
-    
-
 
 call_num = int(input())
 times = list(map(int,input().split()))
