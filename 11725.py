3cbfb73cb184897981e933fdc08ad78bb3dcc964
SR-Kwon <ksr9458@hufs.ac.kr>
2023-09-11 15:01:27 +0900

    9/11

---
+++ 11725.py
@@ -0,0 +1,19 @@
+# 1이 루트인 tree구조 첫재줄에 몇개를 입력 받을지 하고 나머지에 상위, 하위 순으로 연결
+
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+        
+class tree:
+    def __init__(self, head):
+        self.head = head
+        
+    def insert(self, mother, child):
+        if mother == head:
+            current_node = self.head
+                
+head = Node(1)
+binary_tree = NodeMgmt(head)
+
